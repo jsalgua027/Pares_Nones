1f04788ac1062ac89e85b02d893d67705325959e
Nacho <jsalcedoguarde@gmail.com>
2021-11-02 12:29:58 +0100

    Pirmer

--- src/main/java/ParteA.java
+++ src/main/java/ParteA.java
@@ -1,3 +1,7 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,7 +17,39 @@ public class ParteA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner datos=new Scanner (System.in);
+        Random numAleatorio= new Random();
+        
+        int dedosUsuario1;
+        int dedosusuario2;
+        int opcion;
+        
+        do {
+            System.out.println("1 Usuario1 VS Usuario2 ");
+            System.out.println("2 Yo VS Maquina");
+            System.out.println("3 Para terminar");
+
+            opcion=datos.nextInt();
+            
+
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                
+                case 3:
+                    
+                    break;
+               
+                default:
+                    System.out.println("te has equivocado");
+
+            }
+        } while (opcion != 3);
+         
     }
     
 }
